c59ff2ecf094ede809d0aa619ac6a5779dc256dd
Arjun Vasavan <vasavanarjun@gmail.com>
2026-02-28 12:34:53 +0530

    feat: added hasfunction

--- src/hash_table.c
+++ src/hash_table.c
@@ -7,3 +7,21 @@ void create_hash_table(hash_t *hash_table) {
         hash_table[i].head = NULL;
     }
 }
+
+int hash_function(const char *word) {
+
+    if ( word == NULL ) {
+        return 26;
+    }
+
+    char first_character = word[0];
+
+    if ( first_character >= 'a' && first_character <= 'z' ) {
+        return first_character - 'a' ;
+    } else if ( first_character >= 'A' && first_character <= 'Z' ) {
+        first_character = tolower(first_character);
+        return first_character - 'a' ;
+    }
+
+    return 26;
+}
